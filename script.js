9454a28214df532ce101c1668f8556ae58c80f51
Andre <aalbino221@gmail.com>
2021-12-17 20:48:08 -0300

    V2

--- script.js
+++ script.js
@@ -12,16 +12,27 @@
 - se Tesoura && Papel => ganhador = Tesoura 
 if userChoice === ganhador => printe "You win"
 else printe "You lose"*/
-let userChoice = prompt("Escolha entre Pedra, Papel ou Tesoura")
-/*if (userChoice !== "Pedra" && userChoice !== "Papel" && userChoice !== "Tesoura") {alert("Não entendir")} 
-}*/
-let choices = ["Pedra","Papel","Tesoura"]
-function random() {return choices[Math.floor(Math.random()*3)]}
-let compChoice = random()
-function game(userChoice,compChoice) {
+
+
+function input() {
+    let inpu = prompt("Pedra, Papel ou Tesoura?")
+    userChoice = inpu[0].toUpperCase()+(inpu.slice(1)).toLowerCase()
+}
+function random () {
+    let choices = ["Pedra","Papel","Tesoura"]
+    compChoice = choices[Math.floor(Math.random()*3)]}
+
+function gameEngine() {
     if (userChoice == compChoice) {alert ("Empate")}
-    else if (userChoice == "Papel" && compChoice =="Pedra") {alert ("You win")}
-    else if (userChoice == "Pedra" && compChoice =="Tesoura") {alert ("You win")}
-    else if (userChoice =="Tesoura" && compChoice =="Papel"){alert ("You win")}
-    else {alert ("You lose")}
-game (userChoice,compChoice)
+    else if (userChoice == "Papel" && compChoice =="Pedra") {alert ("You win! " + userChoice + " beats " + compChoice)}
+    else if (userChoice == "Pedra" && compChoice =="Tesoura") {alert ("You win! " + userChoice + " beats " + compChoice)}
+    else if (userChoice =="Tesoura" && compChoice =="Papel"){alert  ("You win! " + userChoice + " beats " + compChoice)}
+    else {alert ("You lose! " + compChoice + " beats " + userChoice)}}
+
+
+function game() {
+    input()
+    if (userChoice == "Pedra" || userChoice == "Papel" || userChoice == "Tesoura") {random()
+        gameEngine()}
+    else {alert ("Isso non excsite")}
+    }
