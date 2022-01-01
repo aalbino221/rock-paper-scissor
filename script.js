47cd9a90c3fc5d38a07c78cf8a2125a8ac372260
Andre <aalbino221@gmail.com>
2021-12-31 22:10:34 -0300

    Added UI

--- script.js
+++ script.js
@@ -23,16 +23,48 @@ function random () {
     compChoice = choices[Math.floor(Math.random()*3)]}
 
 function gameEngine() {
-    if (userChoice == compChoice) {alert ("Empate")}
-    else if (userChoice == "Papel" && compChoice =="Pedra") {alert ("You win! " + userChoice + " beats " + compChoice)}
-    else if (userChoice == "Pedra" && compChoice =="Tesoura") {alert ("You win! " + userChoice + " beats " + compChoice)}
-    else if (userChoice =="Tesoura" && compChoice =="Papel"){alert  ("You win! " + userChoice + " beats " + compChoice)}
-    else {alert ("You lose! " + compChoice + " beats " + userChoice)}}
+    if (i === 5) { if (Number(sj) > Number(sp)){endGame('You win!') ; i = 0 ; numPlay.textContent = 0 ; numPC.textContent = 0; sj = 0 ; sp = 0;}
+        else if (Number(sj) < Number(sp)) { endGame('You lose!')  ; i = 0 ; numPlay.textContent = 0 ; numPC.textContent = 0; sj = 0 ; sp = 0;}
+        else {endGame('Empate!') ; i = 0 ; numPlay.textContent = 0 ; numPC.textContent = 0; sj = 0 ; sp = 0;}}
+
+    else if (userChoice == compChoice) {escolhaPlayer.textContent = 'Você escolheu ' + userChoice + ' - Empate';
+    escolhaPC.textContent = 'PC escolheu ' + compChoice + ' - Empate';  escolhaPlayer.style.backgroundColor = ' #a9a9a9'; escolhaPC.style.backgroundColor = ' #a9a9a9'}
+    else if (userChoice == "Papel" && compChoice =="Pedra") {numPlay.textContent = ++sj; ganhador()}
+    else if (userChoice == "Pedra" && compChoice =="Tesoura") {numPlay.textContent = ++sj; ganhador()}
+    else if (userChoice =="Tesoura" && compChoice =="Papel"){numPlay.textContent = ++sj; ganhador()}
+    else {numPC.textContent = ++sp; perdedor()}}
+
+function ganhador() { escolhaPlayer.textContent = 'Você escolheu ' + userChoice + ' - Ganhou';
+    escolhaPC.textContent = 'PC escolheu ' + compChoice + ' - Perdeu'; escolhaPlayer.style.backgroundColor = 'lightgreen'; escolhaPC.style.backgroundColor = '#ed2939' }
+
+function perdedor() { escolhaPlayer.textContent = 'Você escolheu ' + userChoice + ' - Perdeu';
+escolhaPC.textContent = 'PC escolheu ' + compChoice + ' - Ganhou'; escolhaPlayer.style.backgroundColor =  '#ed2939' ; escolhaPC.style.backgroundColor = 'lightgreen'}
+
+function endGame(mensagi) {  main.style.display = 'none'; end.style.display = 'flex';
+    let resultado = document.querySelector('.resultado'); resultado.textContent = `${sj} - ${sp}`;
+    let message = document.querySelector('.message'); message.textContent = mensagi }
 
 
-function game() {
-    input()
-    if (userChoice == "Pedra" || userChoice == "Papel" || userChoice == "Tesoura") {random()
-        gameEngine()}
-    else {alert ("Isso non excsite")}
-    }
+let end = document.querySelector(".end")
+let main = document.querySelector(".main")
+let rock = document.querySelector(".rock")
+let paper = document.querySelector(".paper")
+let scissor = document.querySelector(".scissor")
+let escolhaPlayer = document.querySelector(".escolhaPlayer")
+let escolhaPC = document.querySelector(".escolhaPC")
+let i = 0
+let numPlay = document.querySelector(".numPlay")
+let numPC = document.querySelector(".numPC")
+let sj = Number(numPlay.textContent); /* score do jogador */
+let sp = Number(numPC.textContent); /* score do pc */
+let userChoice = ''
+let body = document.body
+
+rock.addEventListener('click', (e) => {userChoice = "Pedra";random() ; i++ ; gameEngine()})
+
+paper.addEventListener('click', (e) => {userChoice = "Papel" ;random() ; i++ ; gameEngine()})
+
+scissor.addEventListener('click', (e) => {userChoice = "Tesoura" ; random() ; i++ ; gameEngine()})
+
+end.addEventListener('click', (e) => {main.style.display = 'flex'; end.style.display = 'none'; escolhaPlayer.textContent = 'Você escolheu...'; escolhaPC.textContent = 'PC escolheu...'
+ escolhaPlayer.style.backgroundColor = '#e5e5dc' ;  escolhaPC.style.backgroundColor = '#e5e5dc'})
